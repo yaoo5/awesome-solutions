ece84831827ef65d67b26cd8c10efac523782408
Jackie Yao <jackieyao@futunn.com>
2022-06-26 14:56:08 +0800

    feat: use rate-limit middlewares

--- rate-limit/app/router.ts
+++ rate-limit/app/router.ts
@@ -1,13 +1,16 @@
 import { Application } from 'egg';
 import fixedWindowLimit from "./middleware/fixedWindowLimit";
+import slidingWindowLimit from "./middleware/slidingWindowLimit";
+import leakyBucketLimit from "./middleware/leakyBucketLimit";
+import tokenBucketLimit from "./middleware/tokenBucketLimit";
 
 export default (app: Application) => {
   const { controller, router } = app;
 
   router.get('/', controller.home.index);
 
-  router.post('/api/rateLimit/fixedWindow', fixedWindowLimit({}, app), controller.rateLimit.fixedWindow);
-  router.post('/api/rateLimit/slidingWindow', controller.rateLimit.slidingWindow);
-  router.post('/api/rateLimit/leakyBucket', controller.rateLimit.leakyBucket);
-  router.post('/api/rateLimit/tokenBucket', controller.rateLimit.tokenBucket);
+  router.post('/api/rateLimit/fixedWindow', fixedWindowLimit({}), controller.rateLimit.fixedWindow);
+  router.post('/api/rateLimit/slidingWindow', slidingWindowLimit({}), controller.rateLimit.slidingWindow);
+  router.post('/api/rateLimit/leakyBucket', leakyBucketLimit(), controller.rateLimit.leakyBucket);
+  router.post('/api/rateLimit/tokenBucket', tokenBucketLimit(), controller.rateLimit.tokenBucket);
 };
---
+++ rate-limit/typings/app/middleware/index.d.ts
@@ -0,0 +1,17 @@
+// This file is created by egg-ts-helper@1.30.3
+// Do not modify this file!!!!!!!!!
+
+import 'egg';
+import ExportFixedWindowLimit from '../../../app/middleware/fixedWindowLimit';
+import ExportLeakyBucketLimit from '../../../app/middleware/leakyBucketLimit';
+import ExportSlidingWindowLimit from '../../../app/middleware/slidingWindowLimit';
+import ExportTokenBucketLimit from '../../../app/middleware/tokenBucketLimit';
+
+declare module 'egg' {
+  interface IMiddleware {
+    fixedWindowLimit: typeof ExportFixedWindowLimit;
+    leakyBucketLimit: typeof ExportLeakyBucketLimit;
+    slidingWindowLimit: typeof ExportSlidingWindowLimit;
+    tokenBucketLimit: typeof ExportTokenBucketLimit;
+  }
+}
