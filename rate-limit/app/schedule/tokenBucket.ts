21f8382e487f1a4c003170170260422c7c9257a1
Jackie Yao <jackieyao@futunn.com>
2022-06-26 14:58:57 +0800

    fix: token bucket push add limit.

--- rate-limit/app/schedule/tokenBucket.ts
+++ rate-limit/app/schedule/tokenBucket.ts
@@ -4,11 +4,12 @@ module.exports = {
     schedule: {
         interval: '5s',
         type: 'all',
-        disable: false
+        disable: true
     },
     async task(ctx: Context) {
         const { app } = ctx;
+        const max = 5;
 
-        await app.redis.lpush('tokenBucket', Date.now());
+        await app.redis.multi().lpush('tokenBucket', Date.now()).ltrim('tokenBucket', 0, max -1);
     },
 };
